e9abbeb6e3ec9d30f1172ff2eae038031e17d173
Ivan Terentev <makkaroni4@gmail.com>
2025-02-05 15:27:57 +0300

    add attention matrix2

--- attention_test.py
+++ attention_test.py
@@ -4,5 +4,5 @@ from tcv import tcv
 
 model = BertModel.from_pretrained("bert-base-uncased", output_attentions=True)
 
-g = tcv.attention_matrix(model)
+g = tcv.attention_matrix(model, layer=1)
 plt.savefig("a_example.png")
