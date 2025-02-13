3d4291c465089fd97796156fd9f37f84aec353df
IvanTerentev657 <105977081+IvanTerentev657@users.noreply.github.com>
2025-02-13 14:46:26 +0300

    Delete attention3d_test.py

--- attention3d_test.py
+++
@@ -1,7 +0,0 @@
-from transformers import BertModel
-from tcv import tcv
-
-model = BertModel.from_pretrained("bert-base-uncased", output_attentions=True)
-
-fig = tcv.show_3d_output(model, )
-fig.show()
